022699c789109bdd024a4f149e92d5fa5a2aa421
吉祥 <@Lenovo-PC>
2015-11-16 06:10:00 +0900

    http://www-ehentai.org 対応の仮コード

--- src/sumomodownload/TGetHtmlSource.java
+++ src/sumomodownload/TGetHtmlSource.java
@@ -10,9 +10,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class TGetHtmlSource {
+public class TGetHtmlSource extends TDebug{
 	ArrayList<String> list = null;
 	ArrayList<String> imageList = null;
+boolean ehentai=false;
 
 	public TGetHtmlSource(String string) throws IOException {
 		// TODO 自動生成されたコンストラクター・スタブ
@@ -31,29 +32,75 @@ public class TGetHtmlSource {
 		Map<String, List<String>> headers = urlcon.getHeaderFields();
 		Iterator it = headers.keySet().iterator();
 
+		setDebugFlag(false);
 		while (it.hasNext()) {
 			String key = (String) it.next();
-			System.out.println("  " + key + ": " + headers.get(key));
+			dPrintln("  " + key + ": " + headers.get(key));
 
 		}
-		System.out.println(
+		dPrintln(
 				"レスポンスコード[" + urlcon.getResponseCode() + "] " + "レスポンスメッセージ[" + urlcon.getResponseMessage() + "]");
-		System.out.println("\n---- ボディ ----");
+		dPrintln("\n---- ボディ ----");
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(urlcon.getInputStream()));
 		while (true) {
 			String line = br.readLine();
 			if (null == line) {
 				break;
 			}
-			System.out.println(line);
+			dPrintln(line);
 			list.add(line);
 		}
 		br.close();
 		urlcon.disconnect();
-
-		makeURLList();
+		if (string.indexOf("e-hentai.org") > 0) {
+			dPrintln("e-hentai.org site.");
+			makeURLListForHtmlList();	//	ここで、詳細ページのリスト作成が終わる。
+			ehentai=true;
+			
+		} else {
+			makeURLList();
+		}
 	}
 
+	private void makeURLListForHtmlList() {
+		// TODO 自動生成されたメソッド・スタブ
+		setDebugFlag(false);
+		for (int i = 0; i < list.size(); i++) {
+			String line = list.get(i);
+			String elem[] = line.split("\"");
+			// System.out.println("element number: " + elem.length);
+			for (int kk = 0; kk < elem.length; kk++) {
+				// if (kk >0 && elem[kk].indexOf("http://g.e-hentai.org/g/")>=0
+				// && elem[kk-1].indexOf("a href=")>=0){
+				// System.out.println(elem[kk]);
+				// }
+				if (elem[kk].indexOf("http://g.e-hentai.org/s") >= 0 && elem[kk].indexOf("margin") < 0
+						&& elem[kk].indexOf("http://ehgt.org") < 0) {
+					// System.out.println(elem[kk]);
+					try {
+						TPageAnalyzeEHentai obj = new TPageAnalyzeEHentai(elem[kk]);
+						obj.doAnalyze();
+						String lineUrl = obj.getURL();
+						if (null != lineUrl) {
+							imageList.add(lineUrl);
+						}
+					} catch (IOException e) {
+						// TODO 自動生成された catch ブロック
+						e.printStackTrace();
+						System.out.println("ネットワークのエラー");
+						System.exit(-1);
+					}
+
+				}
+			}
+		}
+
+	}
+	public boolean isEHentai(){
+		return ehentai;
+	}
+	
 	public void getList() {
 
 	}
